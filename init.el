97ddc082606c90a7c1e1ba50cdd2a88a62c25e9c
Gavin Mcrey Canete <gavincanete@gmail.com>
2021-06-29 16:50:50 +0900

    Added emacs init config

---
+++ init.el
@@ -0,0 +1,9 @@
+;; Author: Gavin Mcrey Canete
+ 
+
+(icomplete-mode) ;; autocomplete on long-search
+(column-number-mode) ;; used to show the column number
+(global-hl-line-mode 1) ;; shows the current higlighted line globally
+(global-linum-mode 1) ;; shows the code line numbers on left side globally
+(set-face-background 'hl-line "blue") ;; change the color of the highlighted line
+(set-face-foreground 'linum "white")
